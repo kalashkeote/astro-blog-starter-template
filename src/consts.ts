a5697c12863386810911a35139ae13d858b2c938
kalashkeote <kalash.keote@gmail.com>
2026-04-10 08:01:09 -0400

    Update site title and description for branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Future Innovators Franchisee Association";
+export const SITE_DESCRIPTION = "Welcome to the uture Innovators Franchisee Association website!";
